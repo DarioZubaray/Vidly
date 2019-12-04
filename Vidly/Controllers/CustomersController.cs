4fd9c64272aad4c34e3913d9cc40eb6c53dddf6c
dario.zubaray <dzubaray@imagosur.com>
2019-12-04 15:58:44 -0300

    Agregando La vista de Nuevo. Agregando el modelo de Membresia al EntityModels, Se expecifican el nombre de las etiquetas de la clase Cliente a traves de la anotacion Display. Se valida que existan registros en la base para mostrar la tabla de Cliente y peliculas

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
@@ -17,6 +18,16 @@ namespace Vidly.Controllers
         protected override void Dispose(bool disposing)
         {
             this._context.Dispose();
+        }
+
+        public ViewResult New()
+        {
+            var membershipTypes = _context.MembershipTypes.ToList();
+            var viewModel = new NewCustomerViewModel()
+            {
+                MembershipTypes = membershipTypes
+            };
+            return View(viewModel);
         }
 
         public ViewResult Index()
