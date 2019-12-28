97787d9b9af0e7fbcbcec966f857dc32d722edb2
dario zubaray <dzubaray@gmail.com>
2019-12-28 11:37:38 -0300

    Changes to Data Transfer Object (DTO) in the Api, applying AutoMapping and using Newtonsoft.Json for camel notation in dtos

---
+++ Vidly/Dtos/CustomerDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Dtos
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Tell us your name please!")]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        public bool IsSubcribedToNewLetter { get; set; }
+
+        public byte MembershipTypeId { get; set; }
+
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy-MM-dd}")]
+        [Min18IfAMember]
+        public DateTime? Birthdate { get; set; }
+    }
+}
